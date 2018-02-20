7fbaee791de24f7909b13aea229fe6fbdceddd0b
David Fauthoux <david.fauthoux@gmail.com>
2018-02-20 17:00:49 +0100

    cosmetic

--- src/test/java/com/github/itora/crypto/AsymmetricKeysTest.java
+++ src/test/java/com/github/itora/crypto/AsymmetricKeysTest.java
@@ -3,20 +3,15 @@ package com.github.itora.crypto;
 import java.nio.ByteBuffer;
 
 import org.assertj.core.api.Assertions;
-import org.junit.Before;
 import org.junit.Test;
 
 public class AsymmetricKeysTest {
-
-    @Before
-    public void setUp() throws Exception {
-    }
 
     @Test
     public void shouldSignAndVerify() {
     	AsymmetricKey asymmetricKey = AsymmetricKeys.generate();
 
-    	byte[] randomBuffer = new byte[100];
+    	byte[] randomBuffer = new byte[100_000];
     	AsymmetricKeys.RANDOM.nextBytes(randomBuffer);
     	Signature signature = AsymmetricKeys.sign(ByteBuffer.wrap(randomBuffer), asymmetricKey.privateKey());
     	System.out.println(signature);
