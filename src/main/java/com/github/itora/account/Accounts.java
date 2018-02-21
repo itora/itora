e4ec826c25189d27de13a86c7e1aed8b99948e4d
David Fauthoux <david.fauthoux@gmail.com>
2018-02-21 17:22:11 +0100

    ByteArray.from

--- src/main/java/com/github/itora/account/Accounts.java
+++ src/main/java/com/github/itora/account/Accounts.java
@@ -2,11 +2,10 @@ package com.github.itora.account;
 
 import com.github.itora.crypto.PublicKey;
 import com.github.itora.util.ByteArray;
-import com.google.common.io.BaseEncoding;
 
 public class Accounts {
 	private Accounts() {
 	}
 	
-	public static final Account EX_NIHILO = Account.Factory.account(PublicKey.Factory.publicKey(new ByteArray(BaseEncoding.base64().decode("MFwwDQYJKoZIhvcNAQEBBQADSwAwSAJBAKjhnkF7s9i1fZCHj+Wc+x0DdQM2nB/5w2l0ks5oLJVJhEqaWO+c6gZ+UiauWAKrX6iR5MJVawWxJ7OS1KaOjyECAwEAAQ=="))));
+	public static final Account EX_NIHILO = Account.Factory.account(PublicKey.Factory.publicKey(ByteArray.from("MFwwDQYJKoZIhvcNAQEBBQADSwAwSAJBAKjhnkF7s9i1fZCHj+Wc+x0DdQM2nB/5w2l0ks5oLJVJhEqaWO+c6gZ+UiauWAKrX6iR5MJVawWxJ7OS1KaOjyECAwEAAQ==")));
 }
