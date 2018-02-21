b2367255f1d6a1899736cc58e41e5f7f13a4ed88
David Fauthoux <david.fauthoux@gmail.com>
2018-02-21 17:16:07 +0100

    Udp server

---
+++ src/main/java/com/github/itora/server/udp/UdpServer.java
@@ -0,0 +1,107 @@
+package com.github.itora.server.udp;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.nio.ByteBuffer;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.function.Consumer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.davfx.ninio.core.Address;
+import com.davfx.ninio.core.Connecter;
+import com.davfx.ninio.core.Connection;
+import com.davfx.ninio.core.Ninio;
+import com.davfx.ninio.core.Nop;
+import com.davfx.ninio.core.UdpSocket;
+import com.davfx.ninio.util.Wait;
+import com.github.itora.request.RegularSignedPowRequestSerializer;
+import com.github.itora.request.SignedPowRequest;
+import com.github.itora.request.SignedPowRequestSerializer;
+
+public final class UdpServer {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(UdpServer.class);
+
+	private final Connecter connecter;
+	private final ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
+	
+	private final SignedPowRequestSerializer serializer = new RegularSignedPowRequestSerializer();
+	
+	private final Wait waitForClose = new Wait();
+	private final int actualPortToListen;
+
+	public UdpServer(Ninio ninio, int portToListen, Consumer<SignedPowRequest> callback) {
+		if (portToListen == 0) {
+			int port;
+			try {
+				try (ServerSocket ss = new ServerSocket(0)) {
+					port = ss.getLocalPort();
+				}
+			} catch (IOException ioe) {
+				LOGGER.error("Udp server failed", ioe);
+				waitForClose.run();
+				connecter = null;
+				actualPortToListen = 0;
+				return;
+			}
+			actualPortToListen = port;
+		} else {
+			actualPortToListen = portToListen;
+		}
+		
+		LOGGER.debug("Opening server on port {}", actualPortToListen);
+		
+		Wait waitForOpen = new Wait();
+		
+		connecter = ninio.create(UdpSocket.builder().bind(new Address(Address.ANY, actualPortToListen)));
+		connecter.connect(new Connection() {
+			@Override
+			public void failed(IOException ioe) {
+				LOGGER.error("Udp server failed", ioe);
+				waitForOpen.run();
+				waitForClose.run();
+			}
+			@Override
+			public void connected(Address address) {
+				LOGGER.debug("Udp server ready");
+				waitForOpen.run();
+			}
+			@Override
+			public void closed() {
+				LOGGER.debug("Udp server closed");
+				waitForOpen.run();
+				waitForClose.run();
+			}
+			
+			@Override
+			public void received(Address address, ByteBuffer buffer) {
+				LOGGER.trace("Received {} bytes from {}", buffer.remaining(), address);
+				callback.accept(serializer.deserialize(buffer));
+			}
+		});
+
+		waitForOpen.waitFor();
+	}
+	
+	public void close() {
+		if (connecter != null) {
+			connecter.close();
+		}
+		waitForClose.waitFor();
+		executor.shutdown();
+	}
+	
+	public int port() {
+		return actualPortToListen;
+	}
+	
+	public void send(Address address, SignedPowRequest request) {
+		if (connecter == null) {
+			return;
+		}
+		connecter.send(address, serializer.serialize(request), new Nop());
+	}
+}
